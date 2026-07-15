9f8cb45ed037484f5200ff450f5ece6a929088cf
Diego Andrés Quintana Fajardo <147205620+diegoquinfa@users.noreply.github.com>
2026-07-15 11:40:47 -0500

    seo v4

--- src/modules/pages/home/home-link.tsx
+++ src/modules/pages/home/home-link.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import type { ComponentPropsWithoutRef } from 'react'
 
+const SITE_URL = 'https://moflescartagena.com'
+
 type HomeLinkProps = Omit<ComponentPropsWithoutRef<typeof Link>, 'href'>
 
 export function HomeLink({ children, ...rest }: HomeLinkProps) {
   return (
-    <Link href="/" {...rest}>
+    <Link href={`${SITE_URL}/`} {...rest}>
       {children}
     </Link>
   )
